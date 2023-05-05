ce97b5f2583a9b5bff22dc27f75dc0fc514c5852
Fien Yde <fien.yde@student.howest.be>
2023-05-05 14:36:25 +0200

    Monument Detail componetn with mock data

---
+++ src/components/MonumentDetailComponent.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="greetings">
+  {{ monument }}
+    <img src=""/>
+    <h1 class="green">{{ msg }}</h1>
+    <table>
+      <tr>
+        <td>Name</td> //TODO: namen nog veranderen!
+        <td></td>
+      </tr>
+      <tr>
+        <td>Descritpion</td>  //TODO: namen nog veranderen!
+        <td></td>
+      </tr>
+      <tr>
+        <td>Location</td>  //TODO: namen nog veranderen!
+        <td></td>
+      </tr>
+       <tr>
+        <td>Historical Significance</td> //TODO: namen nog veranderen!
+        <td></td>
+      </tr>
+      <tr>
+        <td>Type</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>Year of construction</td>  //TODO: namen nog veranderen!
+        <td></td>
+      </tr>
+      <tr>
+        <td>Monument Designer</td> //TODO: namen nog veranderen!
+        <td></td>
+      </tr>
+      <tr>
+        <td>Accessibility</td>  //TODO: namen nog veranderen!
+        <td></td>
+      </tr>
+      <tr>
+        <td>Used Materialas</td>  //TODO: namen nog veranderen!
+        <td></td>
+      </tr>
+      <tr>
+        <td>Dimensions</td>  //TODO: namen nog veranderen!
+        <td></td>
+      </tr>
+      <tr>
+        <td>Weight</td>  //TODO: namen nog veranderen!
+        <td></td>
+      </tr>
+      <tr>
+        <td>Cost to construct</td>  //TODO: namen nog veranderen!
+        <td></td>
+      </tr>
+      <tr>
+        <td>Audiovisual</td>  //TODO: namen nog veranderen!
+        <td></td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MonumentDetailComponent",
+  props:{
+    monument:{
+      required: true
+    }
+  },
+  data(){
+    return {
+
+    }
+  }
+}
+</script>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <MonumentDetailComponent :monument="monument"/>
   </main>
 </template>
+
+
+
+<script>
+
+import MonumentDetailComponent from '../components/MonumentDetailComponent.vue'
+
+export default{
+  "name": "HomeView",
+  components: { MonumentDetailComponent },
+  data(){
+    return {
+      monument: {
+        name: "monument 1",
+        images: "een url"
+      }
+    }
+  }
+}
+</script>
+
+
