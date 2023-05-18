619981012371a663e4c101b7d867e9be37db84b2
Alexander Vandepitte <vandepittealexander@student.howest.be>
2023-05-18 13:31:45 +0200

    add routing

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
-<template>
-  <div id="app">
-    <router-view />
-  </div>
-</template>
-
-<script>
-export default {
-  name: "App",
-};
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
 </script>
 
-<style>
-@import url("assets/reset.css");
-@import url("assets/main.css");
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/login">Login</router-link></li>
+        <li><router-link to="/register">Register</router-link></li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <router-view />
+  </main>
+</template>
 
+<style scoped>
 #app {
   font-family: Arial, sans-serif;
   display: flex;
@@ -21,5 +25,21 @@ export default {
   align-items: center;
   min-height: 100vh;
   background-color: #f0f0f0;
+}
+
+header {
+  background-color: #333;
+  padding: 10px;
+}
+
+nav {
+  display: flex;
+  justify-content: center;
+}
+
+nav a {
+  color: #fff;
+  text-decoration: none;
+  margin: 0 10px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,48 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import { getMonumentById } from '../api/monumentApi.js'
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/HomeView.vue';
+import Login from '../views/LoginView.vue';
+import Register from '../views/RegisterView.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
+  }
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/monument/:id',
-      name: 'MonumentDetail',
-      component: () => import ('../components/MonumentDetailComponent.vue'),
-      props: route => ({monument: getMonumentById(route.params.id) })
-    },
-    
-    {
-      path: '/newMonument',
-      name: 'newMonument',
-      component: () => import('../views/AddMonumentView.vue')
-    },
+  history: createWebHistory(),
+  routes
+});
 
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('../views/LoginView.vue')
-    },
-
-    {
-      path: '/register',
-      name: 'register',
-      component: () => import('../views/RegisterView.vue')
-    },
-  ]
-})
-
-export default router
+export default router;
