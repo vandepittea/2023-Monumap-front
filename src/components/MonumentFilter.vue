54ef728802d62c47c5883bcaad0189a9178149aa
Alexander Vandepitte <vandepittealexander@student.howest.be>
2023-05-18 14:58:41 +0200

    add MonumentFilter component

---
+++ src/components/MonumentFilter.vue
@@ -0,0 +1,111 @@
+<template>
+    <form class="filter-form" @submit.prevent="filterMonuments">
+      <div class="form-group">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" v-model="filter.name">
+      </div>
+  
+      <div class="form-group">
+        <label for="type">Select the type:</label>
+        <select id="type" name="type" v-model="filter.type">
+          <option value="">Please choose an option</option>
+          <option value="option1">Option 1</option>
+          <option value="option2">Option 2</option>
+          <option value="option3">Option 3</option>
+        </select>
+      </div>
+  
+      <div class="form-group">
+        <label for="yearOfConstruction">Year of construction:</label>
+        <input type="number" id="yearOfConstruction" name="yearOfConstruction" v-model="filter.yearOfConstruction">
+      </div>
+  
+      <div class="form-group">
+        <label for="monumentDesigner">Monument designer:</label>
+        <input type="text" id="monumentDesigner" name="monumentDesigner" v-model="filter.monumentDesigner">
+      </div>
+  
+      <div class="form-group">
+        <label for="costToConstruct">Cost to construct:</label>
+        <input type="number" id="costToConstruct" name="costToConstruct" v-model="filter.costToConstruct">
+      </div>
+  
+      <button type="submit" class="filter-button">Filter</button>
+    </form>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MonumentFilter',
+    data() {
+      return {
+        filter: {
+          name: '',
+          type: '',
+          yearOfConstruction: '',
+          monumentDesigner: '',
+          costToConstruct: '',
+        },
+      };
+    },
+    methods: {
+      filterMonuments() {
+        // TODO: Implement the filtering logic or call the API endpoint
+        // You can access the filter values using `this.filter`
+  
+        // Example code to log the filter values
+        console.log('Filter:', this.filter);
+  
+        // TODO: Update the parent component's `monuments` data with the filtered results
+        // You can emit an event or call a method in the parent component to achieve this
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .filter-form {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 2rem;
+  }
+  
+  .form-group {
+    display: flex;
+    flex-direction: column;
+    margin-right: 1rem;
+    margin-bottom: 1rem;
+  }
+  
+  label {
+    margin-bottom: 0.5rem;
+    font-weight: bold;
+    color: #333;
+  }
+  
+  input[type='text'],
+  input[type='number'],
+  select {
+    padding: 0.5rem;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    font-size: 1rem;
+  }
+  
+  .filter-button {
+    padding: 0.5rem 1rem;
+    background-color: #333;
+    color: #fff;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    font-size: 1rem;
+    transition: background-color 0.3s;
+  }
+  
+  .filter-button:hover {
+    background-color: #555;
+  }
+  </style>  
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,41 +3,9 @@
     <h1>Welcome to MonuMap</h1>
     <h3>Find your favorite Monuments</h3>
 
-    <form class="filter-form" @submit.prevent="filterMonuments">
-      <div class="form-group">
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" v-model="filter.name">
-      </div>
+    <MonumentFilter @filterMonuments="filterMonuments" />
 
-      <div class="form-group">
-        <label for="type">Select the type:</label>
-        <select id="type" name="type" v-model="filter.type">
-          <option value="">Please choose an option</option>
-          <option value="option1">Option 1</option>
-          <option value="option2">Option 2</option>
-          <option value="option3">Option 3</option>
-        </select>
-      </div>
-
-      <div class="form-group">
-        <label for="yearOfConstruction">Year of construction:</label>
-        <input type="number" id="yearOfConstruction" name="yearOfConstruction" v-model="filter.yearOfConstruction">
-      </div>
-
-      <div class="form-group">
-        <label for="monumentDesigner">Monument designer:</label>
-        <input type="text" id="monumentDesigner" name="monumentDesigner" v-model="filter.monumentDesigner">
-      </div>
-
-      <div class="form-group">
-        <label for="costToConstruct">Cost to construct:</label>
-        <input type="number" id="costToConstruct" name="costToConstruct" v-model="filter.costToConstruct">
-      </div>
-
-      <button type="submit" class="filter-button">Filter</button>
-    </form>
-
-    <MonumentList :monuments="monuments" @viewMonumentDetail="viewMonumentDetail" />
+    <MonumentList :monuments="filteredMonuments" @viewMonumentDetail="viewMonumentDetail" />
 
     <img :src="languageImage" @click="toggleLanguage" id="languageImage" alt="Language switcher" />
   </main>
@@ -46,6 +14,7 @@
 <script>
 import MonumentDetailComponent from '../components/MonumentDetailComponent.vue';
 import MonumentList from '../components/MonumentList.vue';
+import MonumentFilter from '../components/MonumentFilter.vue';
 import EnglishFlag from '../assets/images/EnglishFlag.png';
 import DutchFlag from '../assets/images/DutchFlag.png';
 
@@ -54,6 +23,7 @@ export default {
   components: {
     MonumentDetailComponent,
     MonumentList,
+    MonumentFilter,
   },
   data() {
     return {
@@ -94,6 +64,15 @@ export default {
       },
     };
   },
+  computed: {
+    filteredMonuments() {
+      // Apply filtering logic based on the filter object
+      return this.monuments.filter((monument) => {
+        // Example filtering condition: filter by name
+        return monument.name.toLowerCase().includes(this.filter.name.toLowerCase());
+      });
+    },
+  },
   methods: {
     toggleLanguage() {
       this.currentLanguage = this.currentLanguage === 'en' ? 'nl' : 'en';
@@ -103,34 +82,9 @@ export default {
       this.$router.push({ name: 'MonumentDetail', params: { id: monument.id, monument: monument } });
     },
     filterMonuments() {
-      // TODO: Replace with the actual API endpoint URL
-      let url = '/api/monuments?';
-
-      if (this.filter.name) {
-        url += `name=${this.filter.name}&`;
-      }
-      if (this.filter.type) {
-        url += `type=${this.filter.type}&`;
-      }
-      if (this.filter.yearOfConstruction) {
-        url += `yearOfConstruction=${this.filter.yearOfConstruction}&`;
-      }
-      if (this.filter.monumentDesigner) {
-        url += `monumentDesigner=${this.filter.monumentDesigner}&`;
-      }
-      if (this.filter.costToConstruct) {
-        url += `costToConstruct=${this.filter.costToConstruct}&`;
-      }
-
-      // Remove the trailing '&' character
-      url = url.slice(0, -1);
-
-      fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-          this.monuments = data;
-        })
-        .catch((error) => console.error(error));
+      // Apply filtering logic or call the API endpoint here
+      console.log('Filter:', this.filter);
+      // TODO: Update the filteredMonuments computed property based on the filter values
     },
   },
   created() {
@@ -167,51 +121,6 @@ h3 {
   color: #555;
 }
 
-.filter-form {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 2rem;
-}
-
-.form-group {
-  display: flex;
-  flex-direction: column;
-  margin-right: 1rem;
-  margin-bottom: 1rem;
-}
-
-label {
-  margin-bottom: 0.5rem;
-  font-weight: bold;
-  color: #333;
-}
-
-input[type='text'],
-input[type='number'],
-select {
-  padding: 0.5rem;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  font-size: 1rem;
-}
-
-.filter-button {
-  padding: 0.5rem 1rem;
-  background-color: #333;
-  color: #fff;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  font-size: 1rem;
-  transition: background-color 0.3s;
-}
-
-.filter-button:hover {
-  background-color: #555;
-}
-
 #languageImage {
   width: 50px;
   height: 50px;
@@ -219,58 +128,7 @@ select {
   cursor: pointer;
 }
 
-.flexcontainer {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  gap: 1rem;
-  margin-bottom: 3rem;
-}
-
-.flexcontainer > * {
-  width: 30%;
-}
-
 .home-view {
   background-image: linear-gradient(to bottom, #f3f8ff, #c5d9f4);
-}
-
-.filter-form {
-  background-color: #fff;
-  border-radius: 8px;
-  padding: 1.5rem;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-}
-
-.form-group {
-  margin-bottom: 1.5rem;
-}
-
-input[type='text'],
-input[type='number'],
-select {
-  width: 100%;
-}
-
-.filter-button {
-  width: 100%;
-}
-
-.monument-list-item {
-  background-color: #fff;
-  border-radius: 8px;
-  padding: 1.5rem;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  transition: transform 0.3s, box-shadow 0.3s;
-  cursor: pointer;
-}
-
-.monument-list-item:hover {
-  transform: translateY(-3px);
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-}
-
-.flexcontainer {
-  margin-top: 2rem;
 }
 </style>
