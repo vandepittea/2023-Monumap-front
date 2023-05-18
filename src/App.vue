1ddc217c3a482a4b4eb79645cc0134fc85f3dfc0
Alexander Vandepitte <vandepittealexander@student.howest.be>
2023-05-18 13:22:55 +0200

    change the structure of the application; re add app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,25 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
-  <header>
-
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink> 
-        <RouterLink to="/newMonument">Add Monument</RouterLink>
-        <RouterLink to="/register">Register</RouterLink>
-        <RouterLink to="/login">Login</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 
-<style scoped>
+<script>
+export default {
+  name: "App",
+};
+</script>
 
-header{
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  z-index: 1; 
-}
+<style>
+@import url("assets/reset.css");
+@import url("assets/main.css");
 
-nav {
+#app {
+  font-family: Arial, sans-serif;
   display: flex;
-  align-items: center;
   justify-content: center;
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  background-color: var(--color-background);
-  height: 3rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  width: 100%;
-}
-
-/* //TODO: hier aanpassen! */
-@media (min-width: 1024px) { 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: center;
-    font-size: 1rem;
-    padding: 1rem 0;
-    width: 100%;
-  } 
+  align-items: center;
+  min-height: 100vh;
+  background-color: #f0f0f0;
 }
 </style>
